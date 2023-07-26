34096444dd1c30a68174e8ba8b81226a56d80d17
MishaMazniak <Mishamaznyak@gmail.com>
2023-07-26 20:24:50 +0200

    lesson_26

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,41 +1,59 @@
-console.log("_______ les-25 _________");
+console.log("_______ les-26 _________");
 
-// ______ Check the object on datas_________
-function checkOnEmpty(el) {
-  if (Object.keys(el).length === 0) return console.log("Objeck is Empty!!!");
-  return true;
+// 1. Створити конструктор Accumulator.
+// Даний конструктор має приймати число,
+// яке буде початковим значенням. Клас має
+// реалізувати методи increment, при виклику якого
+// передане значення має збільшуватись на 1, та decrement,
+// при виклиці якого значення має зменшуватись.
+
+// 2. Створити конструктор CancelableAccumulator, 
+// який буде наслідувати Accumulator та додатково 
+// буде реалізовувати метод clear, при виклику якого 
+// значення збереженого числа має стати таким, 
+// яким його передали в конструкторі.
+
+function Accumulator(volum) {
+  this.volum = volum;
+  this.decrement = function () {
+    return volum - 1;
+  };
 }
-// _________user_______
-const user = {
-  name: "Misha",
-  age: 32,
-  sayHello: function () {
-    console.log("Привіт, я " + this.name, " мені " + this.age + " років");
-  }
+Accumulator.prototype.increment = function () {
+  return this.volum + 1;
 };
-checkOnEmpty(user); //Check on empty
-if (user.sayHello) user.sayHello(); //Check and run function
-else console.log("Not property - sayHello");
 
-// __________Calculator___________
-const calculator = {
-  num1: "",
-  num2: "",
-  ask: function () {
-    let val1 = +prompt("Enter your first value");
-    let val2 = +prompt("Enter your second value");
-    this.num1 = val1;
-    this.num2 = val2;
-  },
-  sum: function () {
-    console.log(this.num1 + this.num2);
-  },
-  mul: function () {
-    console.log(this.num1 * this.num2);
-  },
-};
-calculator.ask();
-calculator.sum();
-calculator.mul();
+function CancelableAccumulator(volum) {
+  Accumulator.call(this, volum);
+  this.clear = function () {
+    return this.volum;
+  };
+}
+CancelableAccumulator.prototype = Accumulator.prototype;
+
+const liIon = new Accumulator(100);
+const liIon2 = new CancelableAccumulator(72);
 
 
+console.log(liIon);
+console.log(liIon2);
+console.log(liIon.increment());
+console.log(liIon.decrement());
+
+console.log(liIon.volum + "-Ach");
+console.log(liIon2.clear() + "-Ach");
+
+console.log("____________");
+
+function FastLoad(volum, val) {
+  Accumulator.call(this, volum);
+  this.speadLoad = val;
+  this.timeLoad = function () {
+    console.log(this.speadLoad * 60 + " - min");
+  };
+}
+FastLoad.prototype = Accumulator.prototype;
+
+const fastBatery = new FastLoad(84, 2);
+console.log(fastBatery);
+fastBatery.timeLoad();
