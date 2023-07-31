1d4f795804734f44bb7f3c17350639cc38510d6b
MishaMazniak <Mishamaznyak@gmail.com>
2023-07-31 10:33:27 +0200

    practyc

--- javascript/js/les.js
+++ javascript/js/les.js
@@ -1,43 +1,70 @@
-console.log("______ practics on the lesson_25  ________");
-let ob = {
-    name: "Misha",
-    age: 32,
-    number: 123,
-    fullName: {
-        name: "Misha",
-        age: 32,
-        number: 123
-    }
-};
-ob.live = "Wroclaw";
-for (const key in ob) {
-    console.log(key);
-    // console.log(ob[key]);
-    // for (const fullN in ob.fullName) {
-    //   console.log(ob["fullName"][fullN]);
-    // }
-}
-console.log("______________");
+console.log("_______ les-27 _________");
 
-let a = {
-    x: 1,
-    y: 2
-};
-let b = Object.assign({}, a);
-// let b = { ...a };
+let tweets = [
+    {
+        id: "id_tweet",
+        autor: "Jon",
+        title: "text 1",
+        myImg: true,
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident non autem quis voluptate culpa atque omnis voluptas dolore velit reprehenderit fugiat, nihil illo a. Harum, repellendus officia maxime dolorem nostrum et impedit!"
+    },
+    {
+        id: "id_tweet2",
+        autor: "Bob",
+        title: "text 2",
+        myImg: false,
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident non autem quis voluptate culpa atque omnis voluptas dolore velit reprehenderit fugiat, nihil illo a. Harum, repellendus officia maxime dolorem nostrum et impedit!"
+    },
+    {
+        id: "id_tweet3",
+        autor: "Bil",
+        title: "text 3",
+        myImg: false,
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident non autem quis voluptate culpa atque omnis voluptas dolore velit reprehenderit fugiat, nihil illo a. Harum, repellendus officia maxime dolorem nostrum et impedit!"
+    },
+    {
+        id: "id_tweet4",
+        autor: "Alan",
+        title: "text 4",
+        myImg: true,
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident non autem quis voluptate culpa atque omnis voluptas dolore velit reprehenderit fugiat, nihil illo a. Harum, repellendus officia maxime dolorem nostrum et impedit!"
+    },
+    {
+        id: "id_tweet5",
+        autor: "Stiw",
+        title: "text 5",
+        myImg: true,
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident non autem quis voluptate culpa atque omnis voluptas dolore velit reprehenderit fugiat, nihil illo a. Harum, repellendus officia maxime dolorem nostrum et impedit!"
+    },
+    {
+        id: "id_tweet6",
+        autor: "Olaf",
+        title: "text 6",
+        myImg: true,
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident non autem quis voluptate culpa atque omnis voluptas dolore velit reprehenderit fugiat, nihil illo a. Harum, repellendus officia maxime dolorem nostrum et impedit!"
+    },
+];
 
-b.z = 5;
+let myTweets = document.getElementById('my_test_div');
 
-console.log(a);
-console.log(b);
-a.y = null;
-console.log(a);
-for (let key in a) {
-    console.log(a[key]);
-}
-console.log("_______this_______");
+tweets.forEach((el) => {
+    let div = document.createElement("div");
+    div.classList.add('tweet');
+    div.classList.add("tweet_${el.id}");
+    // div.style.color = "red";
 
-// console.log(window);
-// if (innerWidth === 822) {
-//     console.log("Hello");
-// }
+    div.innerHTML = `
+    <h2>
+    ${el.autor}
+    ${el.myImg ? "<img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq20vV8lNZ-19vgbOgiFU_Ooh9z-QnBx2_8V8Jhx0&s" : ""}
+    </h2>
+    <h4>${el.title}</h4>
+    <p>${el.text}</p>
+    `;
+
+    myTweets.append(div);
+});
+
+
+let tweerHTMLList = document.querySelectorAll('.tweet');
+console.log(tweerHTMLList[3].clientWidth);
