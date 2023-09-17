4501a0ec6d7e8d61501ba331ee86b2f091286f0c
MishaMazniak <Mishamaznyak@gmail.com>
2023-09-17 11:13:41 +0200

    lesson-37

---
+++ lesson_37/first-react-app/src/Products.jsx
@@ -0,0 +1,10 @@
+import Product from "./Product"
+import "./css/products.css"
+function Products() {
+  return (
+    <div className="products-div">
+      <Product />
+    </div>
+  )
+}
+export default Products
---
+++ lesson_37/first-react-app/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Header from "./Header.jsx"
+import Products from "./Products.jsx"
+import "./css/index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Header />
+    <Products />
+  </React.StrictMode>
+)
