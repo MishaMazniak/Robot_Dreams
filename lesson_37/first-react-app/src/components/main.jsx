0f34328719bd2869e90990c9d20ee5cce61a10df
MishaMazniak <Mishamaznyak@gmail.com>
2023-09-22 21:43:41 +0200

    lesson_39

--- lesson_37/first-react-app/src/components/main.jsx
+++ lesson_37/first-react-app/src/components/main.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../../node_modules/bootstrap/dist/js/bootstrap.js"
 import Header from "./Header.jsx"
@@ -8,28 +8,17 @@ import Products from "./Products.jsx"
 import "./index.css"
 
 export const App = () => {
-  let tv = {
-    display: 65,
-    model: "SAMSUNG",
-    color: "white",
-    price: 250,
-    img: "src/assets/TV.jpeg"
-  }
-  let laptop = {
-    display: 15.6,
-    model: "ACER",
-    color: "black",
-    price: 500,
-    img: "src/assets/laptop.jpeg"
-  }
-  let xBox = {
-    display: "TV",
-    model: "XBOX",
-    color: "black",
-    price: 350,
-    img: "src/assets/X-Kom.jpeg"
-  }
-  let products = [tv, laptop, xBox]
+  let products = []
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        let fetchProducts = data.products
+        fetchProducts.forEach((product) => {
+          products.push(product)
+        })
+      })
+  }, [])
   const [productsIn, setProducts] = useState(products)
   const [productsInCart, setProductsInCart] = useState([])
 
