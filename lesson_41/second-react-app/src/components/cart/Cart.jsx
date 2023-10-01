2806859f6b774356d8e3e25aae79d061e8359017
MishaMazniak <Mishamaznyak@gmail.com>
2023-10-01 12:37:46 +0200

    lesson_41

--- lesson_41/second-react-app/src/components/cart/Cart.jsx
+++ lesson_41/second-react-app/src/components/cart/Cart.jsx
@@ -1,13 +1,13 @@
 import iconCart from "../../assets/icon-cart.png"
+import {useSelector} from "react-redux"
 import "./cart.css"
 
-function Cart(props) {
+function Cart() {
+  const count = useSelector((state) => state.cart.count)
   return (
     <div className="icon-cart position-absolute top-0 end-0">
       <img src={iconCart} alt="icon-cart"></img>
-      <span className="position-absolute top-0 end-0 number ">
-        {props.myProduct}
-      </span>
+      <span className="position-absolute top-0 end-0 number ">{count}</span>
     </div>
   )
 }
